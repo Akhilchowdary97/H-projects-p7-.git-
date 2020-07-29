eecc4b6f8b11e2433a5b5a3b094b74ccdac838e9
Akhilchowdary97 <67588260+Akhilchowdary97@users.noreply.github.com>
2020-07-29 20:00:07 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse(request,"<h1>welcome my world</h1>")
+def child(request):
+    return render(request,"child.html")
+def home(request):
+    return render(request,"myapp1/home.html",{'name':"Akhil"})
